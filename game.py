cc2fdd6167dc5123afb960f55e229896b95aa3ff
sumedha-student <sumedhap2007@gmail.com>
2025-12-03 18:45:56 +0530

    added game

---
+++ game.py
@@ -0,0 +1,21 @@
+import random
+while True: #LOOP RUNS ALL THE TIME 
+    choice = input('Roll the dice ? (y/n) : ').lower()
+    if choice == 'y':
+        die1 = random.randint(1,6)
+        die2 = random.randint(1,6)
+        print(f'({die1},{die2})')
+    elif choice == 'n':
+        print('Thanks for playing !')
+        break
+    else:
+        print('Invalid Choice !')
+        # User wants the roll die until he enters n = terminate (break if n) if n #So LOOP
+         #ASK:roll the dice? y/n
+         #If y
+         #Generate two random integers in range 1 to 6
+         #If n
+         #Print thank you message
+         #Terminate
+         #Else 
+         #Invalid choice
